abbc8d085d556620c6d3db36c5cda9ef69a0e366
Nouman Nadeem <160357491+NoumanNadeem123@users.noreply.github.com>
2024-03-17 01:13:13 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,9 @@
+// // Question 14
+// //Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make 
+// a list that includes at least three people you’d like to invite to dinner. Then use your list to print 
+// a message to each person, inviting them to dinner. 
+
+let quest_list:string[] =[`Nouman Nadeem `, `Talha Nadeem`, `Muhammad Hussain Ali`];
+for (let i=0; i<quest_list.length; i++){
+    console.log(`Dear Friends ` + quest_list[i] + `, \nwe would like to invite all of you in dinner today.\n\nThank you`)
+}
